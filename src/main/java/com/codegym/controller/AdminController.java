10bae99bbb8bf28c64aabb23fb9d008e0e83f0bf
Quocanh <phiquocanh717@gmail.com>
2021-07-06 01:08:30 +0700

    sdad

--- src/main/java/com/codegym/controller/AdminController.java
+++ src/main/java/com/codegym/controller/AdminController.java
@@ -1,19 +1,19 @@
-package com.codegym.controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-@RestController
-public class AdminController {
-
-    @GetMapping("/admin")
-    public ModelAndView admin(){
-        return new ModelAndView("/admin");
-    }
-
-    @GetMapping("/view")
-    public ModelAndView view(){
-        return new ModelAndView("/view");
-    }
-
-}
+//package com.codegym.controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@RestController
+//public class AdminController {
+//
+//    @GetMapping("/admin")
+//    public ModelAndView admin(){
+//        return new ModelAndView("/admin");
+//    }
+//
+//    @GetMapping("/view")
+//    public ModelAndView view(){
+//        return new ModelAndView("/view");
+//    }
+//
+//}
--- src/main/java/com/codegym/controller/UserController.java
+++ src/main/java/com/codegym/controller/UserController.java
@@ -1,62 +1,62 @@
-//package com.codegym.controller;
-//
-//import com.codegym.model.User;
-//import com.codegym.service.user.IUserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//<<<<<<< HEAD
-//import java.util.Optional;
-//=======
-//
-//@RestController
-//@RequestMapping("/admin")
-//public class UserController {
-//<<<<<<< HEAD
-//    @Autowired
-//    private IUserService userService;
-//
-//    @ModelAttribute("user")
-//    public Iterable<User> users() {
-//        return userService.findAll();
-//    }
-//
-//    @GetMapping("")
-//    public ModelAndView showUser() {
-//        ModelAndView modelAndView = new ModelAndView("/admin");
-//        modelAndView.addObject("user", users());
-//    }
-//
-//    @PostMapping("")
-//    public ResponseEntity<User> saveUser(@RequestBody User user) {
-//        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
-//    }
-//
-//    @GetMapping("listUser")
-//    public ResponseEntity<Iterable<User>> allUser() {
-//        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<User> deleteUser(@PathVariable Long id) {
-//        Optional<User> userOptional = userService.findById(id);
-//        if (!userOptional.isPresent()) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//        userService.remove(id);
-//        return new ResponseEntity<>(userOptional.get(), HttpStatus.NO_CONTENT);
-//    }
-//    @PutMapping("{id}")
-//    public ResponseEntity<User> editUser(@PathVariable Long id,@RequestBody User user){
-//        Optional<User> userOptional = userService.findById(id);
-//                if(!userOptional.isPresent()){
-//                    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//                }
-//                user.setId(id);
-//                return new ResponseEntity<>(userService.save(user),HttpStatus.OK);
-//    }
-//
-//}
+package com.codegym.controller;
+
+import com.codegym.model.User;
+import com.codegym.service.user.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/admin")
+public class UserController {
+
+    @Autowired
+    private IUserService userService;
+
+    @ModelAttribute("user")
+    public Iterable<User> users() {
+        return userService.findAll();
+    }
+
+    @GetMapping("")
+    public ModelAndView showUser() {
+        ModelAndView modelAndView = new ModelAndView("/admin");
+        modelAndView.addObject("user", users());
+        return modelAndView;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping("listUser")
+    public ResponseEntity<Iterable<User>> allUser() {
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+        Optional<User> userOptional = userService.findById(id);
+        if (!userOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        userService.remove(id);
+        return new ResponseEntity<>(userOptional.get(), HttpStatus.NO_CONTENT);
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<User> editUser(@PathVariable Long id,@RequestBody User user){
+        Optional<User> userOptional = userService.findById(id);
+                if(!userOptional.isPresent()){
+                    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                }
+                user.setId(id);
+                return new ResponseEntity<>(userService.save(user),HttpStatus.OK);
+    }
+
+}
