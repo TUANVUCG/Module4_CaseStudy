a7c07d101a8811a573bd8acba43c6335602ed9a9
Quocanh <phiquocanh717@gmail.com>
2021-07-06 08:31:21 +0700

    'sasa'

--- src/main/java/com/codegym/controller/UserController.java
+++ src/main/java/com/codegym/controller/UserController.java
@@ -3,9 +3,6 @@ package com.codegym.controller;
 import com.codegym.model.User;
 import com.codegym.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -59,10 +56,10 @@ public ResponseEntity<Iterable<User>> findAll(){
     }
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
-        Optional<User > userOptional = userService.findById(id);
-        if(!userOptional.isPresent()){
+       Optional<User > userOptional = userService.findById(id);
+       if(!userOptional.isPresent()){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(userOptional.get(),HttpStatus.OK);
+      return new ResponseEntity<>(userOptional.get(),HttpStatus.OK);
     }
 }
