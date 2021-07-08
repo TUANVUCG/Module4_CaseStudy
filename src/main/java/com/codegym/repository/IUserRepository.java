ad9f2c0a402553c1062edf7ff2e9fc20c84393fc
Quocanh <phiquocanh717@gmail.com>
2021-07-08 09:10:05 +0700

    Crud

--- src/main/java/com/codegym/repository/IUserRepository.java
+++ src/main/java/com/codegym/repository/IUserRepository.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
     User findByUsername(String userName);
+    
 }
